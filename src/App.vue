dc3d167f6afbd9d0c81c3f90a8f6dd5e373fcfc4
David Miller <dmiller2606@gmail.com>
2021-03-02 22:41:14 +0100

    Added Name
    Added Background Image and Name to Center

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <navbar/>
-  <landing/>
+  <div class="root">
+    <navbar/>
+    <landing/>
+  </div>
 </template>
 
 <script>
-import landing from './components/landing.vue'
-import navbar from './components/navbar.vue'
+import landing from './components/landing.vue';
+import navbar from './components/navbar.vue';
 
 export default {
   name: 'App',
   components: {
     navbar,
-    landing
-  }
-}
+    landing,
+  },
+};
 </script>
--- src/components/landing.vue
+++ src/components/landing.vue
@@ -1,17 +1,26 @@
 <template>
-  
+  <div>
+    <img class="background" ref="background" src="../assets/background.jpg" />
+    <div class="shadow section" />
+    <div class="section section-1" ref="first">
+      <div>
+        <h1 class="section-1"> David Miller </h1>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import {ref} from 'vue';
 export default {
-  name: 'langing',
-  props: {
-    msg: String
-  },
-  data() {
+  name: 'landing',
+  setup() {
+    const background = ref(null);
+    const first = ref(null);
     return {
-      count: 0
-    }
-  }
-}
+      background,
+      first,
+    };
+  },
+};
 </script>
