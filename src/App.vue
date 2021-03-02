5667515fb94e08727985cde1b99fe2879bdd320f
David Miller <dmiller2606@gmail.com>
2021-03-02 16:22:48 +0100

    Added a navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
+  <navbar/>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import navbar from './components/navbar.vue'
 
 export default {
   name: 'App',
   components: {
+    navbar,
     HelloWorld
   }
 }
